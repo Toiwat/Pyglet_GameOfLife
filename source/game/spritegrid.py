182874ebd64cafea61d8ea58d68919cd3bff54cf
Toiwat <toiwat@gmail.com>
2019-02-26 16:02:25 +0100

    Updated SpriteGrid to have tile_size parameter, for compatibility with GLGrid

--- source/game/spritegrid.py
+++ source/game/spritegrid.py
@@ -3,16 +3,19 @@ import pyglet
 from game.stategrid_np import StateGridNP as StateGrid
 from game import resources
 
+
 class SpriteGrid:
     def __init__(self, stategrid: StateGrid, win_w, win_h):
         self.rows = stategrid.rows
         self.columns = stategrid.columns
         self.sprites = []
 
+        self.tile_size = 16
+
         self.win_w = win_w
         self.win_h = win_h
-        self.grid_h = stategrid.rows * 16
-        self.grid_w = stategrid.columns * 16
+        self.grid_h = stategrid.rows * self.tile_size
+        self.grid_w = stategrid.columns * self.tile_size
 
         self.topleft_x = 0
         self.topleft_y = 0
@@ -34,7 +37,9 @@ class SpriteGrid:
         return (x > self.topleft_x and y < self.topleft_y) and (x < self.bottomright_x and y > self.bottomright_y)
 
     def setup_sprites(self):
-        self.sprites = [[pyglet.sprite.Sprite(img=resources.x16_with_border, x=self.topleft_x+(j*16), y=self.topleft_y-(i*16), batch=self.cells_batch) for j in range(self.columns)]
+        self.sprites = [[pyglet.sprite.Sprite(img=resources.x16_with_border,
+                                              x=self.topleft_x+(j*self.tile_size), y=self.topleft_y-(i*self.tile_size),
+                                              batch=self.cells_batch) for j in range(self.columns)]
                         for i in range(self.rows)]
 
     def update(self, stategrid):
