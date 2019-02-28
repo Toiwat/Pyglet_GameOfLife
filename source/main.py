99b344b1a0d7b4af52e3c80171f84f349376910e
Toiwat <toiwat@gmail.com>
2019-02-28 23:30:30 +0100

    Update main after adding default values.

--- source/main.py
+++ source/main.py
@@ -3,7 +3,7 @@ from game.window import GameOfLife
 
 
 if __name__ == "__main__":
-    window = GameOfLife(1280, 720, 40, 60)
+    window = GameOfLife()
 
     pyglet.clock.schedule_interval(window.update, 1/10)
     pyglet.app.run()
