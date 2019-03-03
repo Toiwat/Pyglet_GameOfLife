a09164360177b0f6a824e83e465cd9008cc6315a
Toiwat <toiwat@gmail.com>
2019-03-03 15:48:39 +0100

    PygletSpritesViewer and PygletOpenglViewer now inherit from common base class Viewer.

--- source/game/ui/pyglet_opengl.py
+++ source/game/ui/pyglet_opengl.py
@@ -1,69 +1,46 @@
-import pyglet
-from game.grid.numpy_grid import NumpyGrid as StateGrid
+from pyglet.gl import GL_QUADS
+from game.grid.stategrid import StateGrid
+from game.ui._base_viewer import Viewer
 
 
-class PygletOpenglViewer:
-    def __init__(self, stategrid: StateGrid, win_w, win_h, tile_size=16):
-        self.rows = stategrid.rows
-        self.columns = stategrid.columns
-
-        self.tile_size = tile_size
-
-        self.win_w = win_w
-        self.win_h = win_h
-        self.grid_h = stategrid.rows * self.tile_size
-        self.grid_w = stategrid.columns * self.tile_size
-
-        self.topleft_x = 0
-        self.topleft_y = 0
-        self.bottomright_x = 0
-        self.bottomright_y = 0
-
-        self.batch = pyglet.graphics.Batch()
+class PygletOpenglViewer(Viewer):
+    def __init__(self, state_grid: StateGrid, window, tile_size=16, color_dead=(255, 255, 255), color_alive=(50,50,50)):
+        # OpenGL vertex and vertex color data
         self.vertex_list = []
         self.color_list = []
-
-        self.setup_canvas_limits()
-        self.setup()
-
-    def setup_canvas_limits(self):
-        self.topleft_x = (self.win_w - self.grid_w) // 2
-        self.topleft_y = (self.win_h + self.grid_h) // 2
-        self.bottomright_x = self.topleft_x + self.grid_w
-        self.bottomright_y = self.topleft_y - self.grid_h
-
-    def position_in_canvas(self, x, y):
-        return (x > self.topleft_x and y < self.topleft_y) and (x < self.bottomright_x and y > self.bottomright_y)
+        super(PygletOpenglViewer, self).__init__(state_grid, window, tile_size, color_alive, color_dead)
 
     def setup(self):
         vertex_data = []
-        for i in range(self.rows):
-            for j in range(self.columns):
+        for i in range(self.state_grid.rows):
+            for j in range(self.state_grid.columns):
                 vertex_data.append(
-                    [self.topleft_x+(j*self.tile_size), self.topleft_y-(i*self.tile_size),
-                     self.topleft_x+(j*self.tile_size), self.topleft_y-(i*self.tile_size) -self.tile_size+1,
-                     self.topleft_x+(j*self.tile_size) +self.tile_size-1, self.topleft_y-(i*self.tile_size) -self.tile_size+1,
-                     self.topleft_x+(j*self.tile_size) +self.tile_size-1, self.topleft_y-(i*self.tile_size)]
+                    [self.top_left.x+(j*self.tile_size), self.top_left.y-(i*self.tile_size),
+                     self.top_left.x+(j*self.tile_size), self.top_left.y-(i*self.tile_size) - self.tile_size+1,
+                     self.top_left.x+(j*self.tile_size) + self.tile_size-1, self.top_left.y-(i*self.tile_size) -self.tile_size+1,
+                     self.top_left.x+(j*self.tile_size) + self.tile_size-1, self.top_left.y-(i*self.tile_size)]
                 )
 
+        vertex_number = self.state_grid.rows * self.state_grid.columns * 4
         vertex_data = [vertex for quad in vertex_data for vertex in quad]
 
-        self.color_list = (255, 255, 255) * self.rows * self.columns * 4
+        # Color list needs to exist as a field to be able to change vertex colors
+        self.color_list = (255, 255, 255) * vertex_number
         self.vertex_list = self.batch.add(
-            self.rows * self.columns * 4,
-            pyglet.gl.GL_QUADS,
+            vertex_number,
+            GL_QUADS,
             None,
             ('v2i', vertex_data),
             ('c3B', self.color_list)
         )
 
-    def update(self, stategrid):
-        for cell in stategrid.updated:
+    def update(self):
+        for cell in self.state_grid.updated:
             i = cell[0]
             j = cell[1]
-            vert_index = (self.columns * 4 * i) + j * 4
-            color_index = vert_index * 3
-            if stategrid.cells[i][j] == 1:
+            vertex_index = (self.state_grid.columns * 4 * i) + j * 4
+            color_index = vertex_index * 3
+            if self.state_grid.cells[i][j] == 1:
                 self.vertex_list.colors[color_index:color_index+12] = [50, 50, 50]*4
             else:
                 self.vertex_list.colors[color_index:color_index + 12] = [255, 255, 255] * 4
--- source/game/ui/pyglet_sprites.py
+++ source/game/ui/pyglet_sprites.py
@@ -1,51 +1,25 @@
-import pyglet
-# from game.stategrid import ArrayGrid
-from game.grid.numpy_grid import NumpyGrid as StateGrid
+from pyglet.sprite import Sprite
+from game.grid.stategrid import StateGrid
 from game import resources
+from game.ui._base_viewer import Viewer
 
 
-class PygletSpritesViewer:
-    def __init__(self, stategrid: StateGrid, win_w, win_h):
-        self.rows = stategrid.rows
-        self.columns = stategrid.columns
+class PygletSpritesViewer(Viewer):
+    def __init__(self, state_grid: StateGrid, window, color_dead=(255, 255, 255), color_alive=(50,50,50)):
+        # List for pyglet sprites
         self.sprites = []
+        super(PygletSpritesViewer, self).__init__(state_grid, window, tile_size=16, color_alive=color_alive, color_dead=color_dead)
 
-        self.tile_size = 16
+    def setup(self):
+        self.sprites = [[Sprite(img=resources.x16_with_border,
+                                x=self.top_left.x+(j*self.tile_size), y=self.top_left.y-(i*self.tile_size),
+                                batch=self.batch)
+                         for j in range(self.state_grid.columns)] for i in range(self.state_grid.rows)]
 
-        self.win_w = win_w
-        self.win_h = win_h
-        self.grid_h = stategrid.rows * self.tile_size
-        self.grid_w = stategrid.columns * self.tile_size
-
-        self.topleft_x = 0
-        self.topleft_y = 0
-        self.bottomright_x = 0
-        self.bottomright_y = 0
-
-        self.cells_batch = pyglet.graphics.Batch()
-
-        self.setup_canvas_limits()
-        self.setup_sprites()
-
-    def setup_canvas_limits(self):
-        self.topleft_x = (self.win_w - self.grid_w) // 2
-        self.topleft_y = (self.win_h + self.grid_h) // 2
-        self.bottomright_x = self.topleft_x + self.grid_w
-        self.bottomright_y = self.topleft_y - self.grid_h
-
-    def position_in_canvas(self, x, y):
-        return (x > self.topleft_x and y < self.topleft_y) and (x < self.bottomright_x and y > self.bottomright_y)
-
-    def setup_sprites(self):
-        self.sprites = [[pyglet.sprite.Sprite(img=resources.x16_with_border,
-                                              x=self.topleft_x+(j*self.tile_size), y=self.topleft_y-(i*self.tile_size),
-                                              batch=self.cells_batch) for j in range(self.columns)]
-                        for i in range(self.rows)]
-
-    def update(self, stategrid):
-        for i in range(self.rows):
-            for j in range(self.columns):
-                if stategrid.cells[i][j] == 1:
-                    self.sprites[i][j].color = (255, 200, 150)
-                elif stategrid.cells[i][j] == 0:
-                    self.sprites[i][j].color = (255, 255, 255)
+    def update(self):
+        for i in range(self.state_grid.rows):
+            for j in range(self.state_grid.columns):
+                if self.state_grid.cells[i][j] == 1:
+                    self.sprites[i][j].color = self.color_alive
+                elif self.state_grid.cells[i][j] == 0:
+                    self.sprites[i][j].color = self.color_dead
